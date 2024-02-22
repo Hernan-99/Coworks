c46a8eeb409be83e1db4f4d97df8b79485555a84
Hernan <hernan24744@gmail.com>
2024-02-21 22:06:22 -0300

    Funcionalidad de event listener para boton de redireccion al registro

--- app/src/main/java/com/example/coworks/LoginActivity.java
+++ app/src/main/java/com/example/coworks/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.coworks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView registrarse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login);
+
+        registrarse = findViewById(R.id.registrarse);
+        registrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Creacion de Intent para abrir la activity
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
