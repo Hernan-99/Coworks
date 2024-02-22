1f407a1f9a2e19625bc405c6931aac2766ecdc3f
Hernan <hernan24744@gmail.com>
2024-02-21 22:00:36 -0300

    Vista para la interfaz de registro

--- app/src/main/java/com/example/coworks/InicioActivity.java
+++ app/src/main/java/com/example/coworks/InicioActivity.java
@@ -1,14 +1,12 @@
 package com.example.coworks;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login);
     }
 }
