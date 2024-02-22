32dae6d6e84691e0d0adf1745b66a0243052732f
Hernan <hernan24744@gmail.com>
2024-02-22 14:55:04 -0300

    Implementacin de funcion OcultarActionBar y logica para redireccionar activities

--- app/src/main/java/com/example/coworks/LoginActivity.java
+++ app/src/main/java/com/example/coworks/LoginActivity.java
@@ -6,17 +6,25 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView registrarse;
+    Button ingresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //funcion para ocultar barras
+        OcultarActionBar.ocultar(this);
+
         registrarse = findViewById(R.id.registrarse);
+        ingresar = findViewById(R.id.ingresar);
+
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,5 +34,12 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intet = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intet);
+            }
+        });
     }
 }
