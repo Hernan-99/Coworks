e96247952ca975ce022ec032debf76dbc539986f
Hernan <hernan24744@gmail.com>
2024-02-21 22:05:31 -0300

    funcionalidades para redireccionar al login y signup

--- app/src/main/java/com/example/coworks/InicioActivity.java
+++ app/src/main/java/com/example/coworks/InicioActivity.java
@@ -1,12 +1,39 @@
 package com.example.coworks;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class InicioActivity extends AppCompatActivity {
+
+    Button btnLogin, btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_inicio);
+
+        btnLogin = findViewById(R.id.btnLoginActivity);
+        btnSignUp = findViewById(R.id.btnSignUpActivity);
+
+        // Configuracion de evento click para los botones
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Creacion de Intent para abrir la activity
+                Intent intent = new Intent(InicioActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Creacion de Intent para abrir la activity
+                Intent intent = new Intent(InicioActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
