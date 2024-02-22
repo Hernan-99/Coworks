693300d68c73dec5b3d8aa37b1137c6e8b63b990
Hernan <hernan24744@gmail.com>
2024-02-22 14:53:01 -0300

    Implementacion de la funcion OcultarActionBar

--- app/src/main/java/com/example/coworks/SignupActivity.java
+++ app/src/main/java/com/example/coworks/SignupActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
@@ -15,6 +16,9 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //funcion para ocultar barras
+        OcultarActionBar.ocultar(this);
 
         login = findViewById(R.id.iniciarSesion);
         login.setOnClickListener(new View.OnClickListener() {
