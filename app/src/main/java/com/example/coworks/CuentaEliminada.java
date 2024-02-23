9d8170345701a6c6b479700a4a89a8428f92089e
Hernan <hernan24744@gmail.com>
2024-02-23 20:59:35 -0300

    Archivos de imagen, bordes, backgrounds, shapes

---
+++ app/src/main/java/com/example/coworks/CuentaEliminada.java
@@ -0,0 +1,14 @@
+package com.example.coworks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CuentaEliminada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuenta_eliminada);
+    }
+}
---
+++ app/src/main/java/com/example/coworks/SuccessLugarReservado.java
@@ -0,0 +1,14 @@
+package com.example.coworks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessLugarReservado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_lugar_reservado);
+    }
+}
