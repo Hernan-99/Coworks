ba1c9f807b0716352905dccaf484a7201b25b4a8
Hernan <hernan24744@gmail.com>
2024-02-23 21:18:29 -0300

    Clase para confirmar eliminacion de cuenta y logica para redireccionamiento de activities

--- app/src/main/java/com/example/coworks/ConfirmarEliminarCuenta.java
+++ app/src/main/java/com/example/coworks/ConfirmarEliminarCuenta.java
@@ -2,13 +2,37 @@ package com.example.coworks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ConfirmarEliminarCuenta extends AppCompatActivity {
+    private Button btnConfirmar, btnCancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmar_eliminar_cuenta);
+
+        btnConfirmar = findViewById(R.id.btnConfirmar);
+        btnCancelar = findViewById(R.id.btnCancelar);
+
+        btnConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ConfirmarEliminarCuenta.this, CuentaEliminada.class);
+                startActivity(intent);
+                Toast.makeText(ConfirmarEliminarCuenta.this, "Cuenta eliminada", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ConfirmarEliminarCuenta.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
