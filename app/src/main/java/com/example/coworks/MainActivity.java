1bfaa2a09eb8d000e3ebfa849ddc26695ddc453f
Hernan <hernan24744@gmail.com>
2024-02-23 21:20:31 -0300

    implementacion de interface para el redireccionamiento con bottomnav

--- app/src/main/java/com/example/coworks/MainActivity.java
+++ app/src/main/java/com/example/coworks/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.coworks;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.coworks.interfaces.RedireccionamientoNavbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
 
@@ -20,10 +26,41 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<CardModel> cardModelArrayList;
     private CardAdapter cardAdapter;
 
+    BottomNavigationView nav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        nav = findViewById(R.id.bottom_navbar);
+        RedireccionamientoNavbar listener = new RedireccionamientoNavbar(this);
+        nav.setOnItemSelectedListener(listener);
+
+/*
+PASAR A UNA INTERFACE
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                pasar swit a if-else
+                int itemId = item.getItemId();
+                if(itemId == R.id.home){
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.add) {
+                    Toast.makeText(MainActivity.this, "Add", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.perfil) {
+                    Intent intent = new Intent(MainActivity.this, PerfilActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(MainActivity.this, "Perfil", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });*/
+
+        //NavigationBarView.OnItemSelectedListener()
 
         //funcion para ocultar barras
         OcultarActionBar.ocultar(this);
@@ -99,4 +136,5 @@ public class MainActivity extends AppCompatActivity {
         //viewPager2.setPadding(100, 0, 100, 0);
 
     }
+
 }
