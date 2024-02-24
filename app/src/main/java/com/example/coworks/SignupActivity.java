3def1d4437e490b57d8a513d3a49d688c92677bc
Hernan <hernan24744@gmail.com>
2024-02-23 21:25:01 -0300

    logica para redireccionamiento de activities

--- app/src/main/java/com/example/coworks/SignupActivity.java
+++ app/src/main/java/com/example/coworks/SignupActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
 
-    TextView login;
+    private TextView login;
+    private Button btnRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,15 @@ public class SignupActivity extends AppCompatActivity {
 
         //funcion para ocultar barras
         OcultarActionBar.ocultar(this);
+
+        btnRegistrarse = findViewById(R.id.btnRegistrarse);
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
         login = findViewById(R.id.iniciarSesion);
         login.setOnClickListener(new View.OnClickListener() {
