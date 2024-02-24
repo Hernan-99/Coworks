25bc572ee252000f51332c83a0f121fcd663d384
Hernan <hernan24744@gmail.com>
2024-02-23 21:17:36 -0300

    clase para los detalles de las cards

--- app/src/main/java/com/example/coworks/CardDetallesActivity.java
+++ app/src/main/java/com/example/coworks/CardDetallesActivity.java
@@ -1,2 +1,40 @@
-package com.example.coworks;public class CardDetallesActivity {
+package com.example.coworks;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.coworks.interfaces.RedireccionamientoNavbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class CardDetallesActivity extends AppCompatActivity {
+    Button btnReserva;
+    BottomNavigationView nav;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_detalles);
+
+        nav = findViewById(R.id.bottom_navbar);
+        RedireccionamientoNavbar listener = new RedireccionamientoNavbar(this);
+        nav.setOnItemSelectedListener(listener);
+
+        //funcion para ocultar barras
+        OcultarActionBar.ocultar(this);
+
+        btnReserva = findViewById(R.id.btnReserva);
+        btnReserva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CardDetallesActivity.this, ReservaActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
