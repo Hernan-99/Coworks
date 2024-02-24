707533b4b43c52d47c40204591b5c74ad3a14290
Hernan <hernan24744@gmail.com>
2024-02-23 21:16:37 -0300

    Adapter para la carga de tarjetas

--- app/src/main/java/com/example/coworks/CardAdapter.java
+++ app/src/main/java/com/example/coworks/CardAdapter.java
@@ -1,6 +1,7 @@
 package com.example.coworks;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -62,8 +63,14 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+
                     //cambiar logica para cuando se haga un touch en la card mostremos una vista
                     // que muestre los datos de esa card en base a un get por id
+
+                    Intent intent = new Intent(context, CardDetallesActivity.class);
+                    context.startActivity(intent);
+
+
                     Toast.makeText(context, "redireccionamos", Toast.LENGTH_SHORT).show();
                 }
             });
