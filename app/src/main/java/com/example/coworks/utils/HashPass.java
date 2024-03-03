18c568b15730c6f8ad19b740073f95d02e9004d5
Hernan <hernan24744@gmail.com>
2024-03-03 14:04:21 -0300

    clase para agregar seguridad a la app medicante el hasheo de contraseña bajo algoritmo SHA-256

--- app/src/main/java/com/example/coworks/utils/HashPass.java
+++ app/src/main/java/com/example/coworks/utils/HashPass.java
@@ -4,19 +4,21 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashPass {
-    public static String hashPassword(String password) {
-        try {
+
+    public static String hashPassword(String password){
+        try{
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] encodedhash = digest.digest(password.getBytes());
-            return bytesToHex(encodedhash);
-        } catch (NoSuchAlgorithmException e) {
+            byte[] encodeHash = digest.digest(password.getBytes());
+            return bytesHex(encodeHash);
+        } catch (NoSuchAlgorithmException e){
             e.printStackTrace();
             return null;
         }
     }
 
-    private static String bytesToHex(byte[] hash) {
+    private static String bytesHex(byte[] hash){
         StringBuilder hexString = new StringBuilder(2 * hash.length);
+
         for (byte b : hash) {
             String hex = Integer.toHexString(0xff & b);
             if (hex.length() == 1) {
@@ -26,4 +28,5 @@ public class HashPass {
         }
         return hexString.toString();
     }
+
 }
