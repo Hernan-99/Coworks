42bd5921a27d7a6c965b45a6eff5cb2faa8c8d0a
Hernan <hernan24744@gmail.com>
2024-03-03 14:12:36 -0300

    logica la creacion de usuario con rol de cliente

--- app/src/main/java/com/example/coworks/ui/registroclient/RegistroClientActivity.java
+++ app/src/main/java/com/example/coworks/ui/registroclient/RegistroClientActivity.java
@@ -1,6 +1,7 @@
 package com.example.coworks.ui.registroclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,22 +11,32 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.coworks.data.database.appdatabase.CoworksDatabase;
+import com.example.coworks.data.database.dao.RolDao;
+import com.example.coworks.data.database.dao.UsuarioDao;
+import com.example.coworks.data.database.model.Rol;
+import com.example.coworks.data.database.model.Usuario;
 import com.example.coworks.ui.registroadmin.RegistroAdminActivity;
 import com.example.coworks.ui.login.LoginActivity;
+import com.example.coworks.utils.HashPass;
 import com.example.coworks.utils.OcultarActionBar;
 import com.example.coworks.R;
 
 public class RegistroClientActivity extends AppCompatActivity {
 
-    EditText nombre, telefono, email, password;
-
+    private EditText nombre, telefono, email, password;
     private TextView login, adminRegistro;
     private Button btnRegistrarse;
+
+    private UsuarioDao usuarioDao;
+    private RolDao rolDao;
+    private RegistroClientViewModel registroClientViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_registro_client);
 
         //funcion para ocultar barras
         OcultarActionBar.ocultar(this);
@@ -36,18 +47,19 @@ public class RegistroClientActivity extends AppCompatActivity {
         password = findViewById(R.id.iptPassword);
         btnRegistrarse = findViewById(R.id.btnRegistrarse);
         adminRegistro = findViewById(R.id.registroAdmin);
+        login = findViewById(R.id.iniciarSesion);
 
+        usuarioDao = CoworksDatabase.getInstance(this).usuarioDao();
+        rolDao =CoworksDatabase.getInstance(this).rolDao();
+        registroClientViewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(RegistroClientViewModel.class);
+        registroClientViewModel.usuarioDao = CoworksDatabase.getInstance(this).usuarioDao();
         btnRegistrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(RegistroClientActivity.this, "Usuario creado, inicie sesion", Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent(RegistroClientActivity.this, LoginActivity.class);
-                startActivity(intent);
+                insertClient();
             }
         });
 
-        login = findViewById(R.id.iniciarSesion);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,6 +75,37 @@ public class RegistroClientActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
+    private void insertClient(){
+        String nombreUsr = nombre.getText().toString();
+        String telefonoUsr = telefono.getText().toString();
+        String emailUsr = email.getText().toString();
+        String passwordUsr = password.getText().toString();
+
+        if (!nombreUsr.isEmpty() && !telefonoUsr.isEmpty() && !emailUsr.isEmpty() && !passwordUsr.isEmpty()){
+            // --------------- si los campos no estan vacios, entonces: --------------
+            //establezco rol
+            Rol rolClient = new Rol("ROL_CLIENT");
+            rolDao.postRolAsync(rolClient);
+
+            //hasheo contraseña
+            String hashedPassword = HashPass.hashPassword(passwordUsr); //contraseña hasheada
+
+            Usuario usuario = new Usuario();
+            usuario.setNombre_usuario(nombreUsr);
+            usuario.setTelefono_usuario(telefonoUsr);
+            usuario.setEmail_usuario(emailUsr);
+            usuario.setPassword_usuario(hashedPassword);
+            usuario.setRolIdRol(1);
+
+            registroClientViewModel.postUsuario(usuario);
+
+            Intent intent = new Intent(RegistroClientActivity.this, LoginActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, "Usuario registrado, inicie sesion", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/coworks/ui/registroclient/RegistroClientViewModel.java
+++ app/src/main/java/com/example/coworks/ui/registroclient/RegistroClientViewModel.java
@@ -1,4 +1,21 @@
 package com.example.coworks.ui.registroclient;
 
-public class RegistroClientViewModel {
+import androidx.lifecycle.ViewModel;
+
+import com.example.coworks.data.database.dao.UsuarioDao;
+import com.example.coworks.data.database.model.Usuario;
+
+public class RegistroClientViewModel extends ViewModel {
+    public UsuarioDao usuarioDao;
+
+    public RegistroClientViewModel() {
+    }
+
+    public RegistroClientViewModel(UsuarioDao usuarioDao) {
+        this.usuarioDao = usuarioDao;
+    }
+
+    public void postUsuario(Usuario usuario){
+        usuarioDao.postUserAsync(usuario);
+    }
 }
