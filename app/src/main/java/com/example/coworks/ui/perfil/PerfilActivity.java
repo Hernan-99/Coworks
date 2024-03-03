7da557ab21497a96b189ff6f01f5153855843d17
Hernan <hernan24744@gmail.com>
2024-03-03 14:10:23 -0300

    Logica para los listeners, lectura de datos para el usuario en perfil

--- app/src/main/java/com/example/coworks/ui/perfil/PerfilActivity.java
+++ app/src/main/java/com/example/coworks/ui/perfil/PerfilActivity.java
@@ -1,9 +1,15 @@
 package com.example.coworks.ui.perfil;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,8 +18,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.coworks.ConfirmarEliminarCuenta;
+import com.example.coworks.data.database.appdatabase.CoworksDatabase;
+import com.example.coworks.data.database.dao.UsuarioDao;
+import com.example.coworks.data.database.model.Usuario;
 import com.example.coworks.ui.MainActivity;
+import com.example.coworks.ui.fragment.EditarPerfilFragment;
 import com.example.coworks.ui.inicio.InicioActivity;
+import com.example.coworks.utils.HashPass;
 import com.example.coworks.utils.OcultarActionBar;
 import com.example.coworks.R;
 import com.example.coworks.utils.RedireccionamientoNavbar;
@@ -24,16 +35,28 @@ public class PerfilActivity extends AppCompatActivity {
     // variables
     private TextView iptNombre, iptTelefono, iptEmail, iptPassword;
     private TextView bntEliminarCuenta, btnCerrarSesion;
-    private Button btnGuardarDatos, btnCancelar;
+    private Button btnEditar;
+    private PerfilViewModel perfilViewModel;
     private BottomNavigationView nav;
+
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        perfilViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(PerfilViewModel.class);
+        perfilViewModel.usuarioDao = CoworksDatabase.getInstance(this).usuarioDao();
+
+        // Inicializar SharedPreferences
+        sharedPreferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+
+
         //funcion para ocultar barras
         OcultarActionBar.ocultar(this);
 
-
+        //perfilViewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(PerfilViewModel.class);
+        //perfilViewModel.usuarioDao = CoworksDatabase.getInstance(this).usuarioDao();
 
         nav = findViewById(R.id.bottom_navbar);
         RedireccionamientoNavbar listener = new RedireccionamientoNavbar(this);
@@ -49,27 +72,36 @@ public class PerfilActivity extends AppCompatActivity {
         iptTelefono = findViewById(R.id.iptTelefono);
         iptEmail = findViewById(R.id.iptEmail);
         iptPassword = findViewById(R.id.iptPassword);
-        btnGuardarDatos = findViewById(R.id.btnGuardarDatos);
-        btnCancelar = findViewById(R.id.btnCancelar);
+        //btnEditar = findViewById(R.id.btnEditarPerfil);
         bntEliminarCuenta = findViewById(R.id.btnEliminarCuenta);
         btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
         //-------------------------------
 
 
-        // -------- LISTENERS BTNS ---------
-        btnGuardarDatos.setOnClickListener(new View.OnClickListener() {
+
+        String email = obtenerEmailUsuarioSesion();
+        String password = obtenerContraseñaUsuarioSesion();
+        perfilViewModel.getUsuarioCredenciales(email, HashPass.hashPassword(password)).observe(this, new Observer<Usuario>() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(PerfilActivity.this, MainActivity.class);
-                startActivity(intent);
-                Toast.makeText(PerfilActivity.this, "Datos guardados correctamente", Toast.LENGTH_SHORT).show();
+            public void onChanged(Usuario usuario) {
+                if (usuario != null) {
+                    // Actualizar la interfaz de usuario con los nuevos datos del usuario
+                    iptNombre.setText(usuario.getNombre_usuario());
+                    iptTelefono.setText(usuario.getTelefono_usuario());
+                    iptEmail.setText(usuario.getEmail_usuario());
+                    iptPassword.setText("********");
+                } else {
+                    // si no se encuentra usuario
+                    Toast.makeText(PerfilActivity.this, "Error al cargar los datos", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-        btnCancelar.setOnClickListener(new View.OnClickListener() {
+
+        // -------- LISTENERS BTNS ---------
+        btnEditar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(PerfilActivity.this, MainActivity.class);
-                startActivity(intent);
+                editarPerfilDialog();
             }
         });
 
@@ -92,6 +124,49 @@ public class PerfilActivity extends AppCompatActivity {
             }
         });
         // ----------------------------------
+        mostrarDatosUsuario();
+    }
+
+    private void editarPerfilDialog(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        EditarPerfilFragment editarPerfilFragment = new EditarPerfilFragment();
+        //editarPerfilFragment.show(fragmentManager,"editar_perfil_dialog");
+
+        //carga de datos en el fragment
+        String nombre = iptNombre.getText().toString();
+        String telefono = iptTelefono.getText().toString();
+        String email = iptEmail.getText().toString();
+        String password= iptPassword.getText().toString();
+
+        // Crear un Bundle para pasar los datos como argumentos al fragmento
+        Bundle bundle = new Bundle();
+        bundle.putString("nombre", nombre);
+        bundle.putString("telefono", telefono);
+        bundle.putString("email", email);
+        bundle.putString("password", password);
+
+        // Establecer los argumentos en el fragmento
+        editarPerfilFragment.setArguments(bundle);
+        editarPerfilFragment.show(fragmentManager, "editar_perfil_dialog");
+    }
+
+    private void mostrarDatosUsuario() {
+        // Obtener email y contraseña del usuario del shared preferences
+        String email = obtenerEmailUsuarioSesion();
+        String password = obtenerContraseñaUsuarioSesion();
+
+        // Obtencion de usuario de la base de datos
+        perfilViewModel.getUsuarioCredenciales(email, HashPass.hashPassword(password));
+    }
+
+    private String obtenerEmailUsuarioSesion() {
+        // Obtener el email de usuario almacenado en SharedPreferences
+        return sharedPreferences.getString("email", ""); // Si no se encuentra, devuelve una cadena vacía
+    }
+
+    private String obtenerContraseñaUsuarioSesion() {
+        // Obtener la contraseña almacenada en SharedPreferences
+        return sharedPreferences.getString("password", ""); // Si no se encuentra, devuelve una cadena vacía
     }
 
 
--- app/src/main/java/com/example/coworks/ui/perfil/PerfilViewModel.java
+++ app/src/main/java/com/example/coworks/ui/perfil/PerfilViewModel.java
@@ -1,4 +1,22 @@
 package com.example.coworks.ui.perfil;
 
-public class PerfilViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.coworks.data.database.dao.UsuarioDao;
+import com.example.coworks.data.database.model.Usuario;
+
+public class PerfilViewModel extends ViewModel {
+    public UsuarioDao usuarioDao;
+    public PerfilViewModel() {
+    }
+
+    public PerfilViewModel(UsuarioDao usuarioDao) {
+        this.usuarioDao = usuarioDao;
+    }
+
+    public LiveData<Usuario> getUsuarioCredenciales(String email, String password){
+        return usuarioDao.getUsuarioCredenciales(email, password);
+    }
+
 }
