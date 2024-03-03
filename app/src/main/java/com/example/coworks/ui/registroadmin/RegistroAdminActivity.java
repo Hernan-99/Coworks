a872a6e8600948e593cce3af0e454ca53b8d4fd6
Hernan <hernan24744@gmail.com>
2024-03-03 14:11:59 -0300

    logica la creacion de usuario con rol de administrador

--- app/src/main/java/com/example/coworks/ui/registroadmin/RegistroAdminActivity.java
+++ app/src/main/java/com/example/coworks/ui/registroadmin/RegistroAdminActivity.java
@@ -1,6 +1,7 @@
 package com.example.coworks.ui.registroadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,17 +12,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.coworks.R;
+import com.example.coworks.data.database.appdatabase.CoworksDatabase;
+import com.example.coworks.data.database.dao.RolDao;
+import com.example.coworks.data.database.dao.UsuarioDao;
+import com.example.coworks.data.database.model.Rol;
+import com.example.coworks.data.database.model.Usuario;
 import com.example.coworks.ui.login.LoginActivity;
+import com.example.coworks.utils.HashPass;
 
 public class RegistroAdminActivity extends AppCompatActivity {
     private Button btnRegistroAdmin;
     private TextView btnIniciar;
     private EditText nombre, telefono, email, password;
 
+    private UsuarioDao usuarioDao;
+    private RolDao rolDao;
+    private RegistroAdminViewModel registroAdminViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup_admin);
+        setContentView(R.layout.activity_registro_admin);
 
         btnRegistroAdmin = findViewById(R.id.btnRegistro);
         btnIniciar = findViewById(R.id.btnIniciar);
@@ -32,12 +43,14 @@ public class RegistroAdminActivity extends AppCompatActivity {
         email = findViewById(R.id.iptEmailAdmin);
         password = findViewById(R.id.iptPassAdmin);
 
+        usuarioDao = CoworksDatabase.getInstance(this).usuarioDao();
+        rolDao = CoworksDatabase.getInstance(this).rolDao();
+        registroAdminViewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(RegistroAdminViewModel.class);
+
         btnRegistroAdmin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(RegistroAdminActivity.this, "Admin creado, inicie sesion", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(RegistroAdminActivity.this, LoginActivity.class);
-                startActivity(intent);
+                insertAdmin();
             }
         });
         btnIniciar.setOnClickListener(new View.OnClickListener() {
@@ -47,5 +60,37 @@ public class RegistroAdminActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void insertAdmin(){
+        String nombreUsr = nombre.getText().toString();
+        String telefonoUsr = telefono.getText().toString();
+        String emailUsr = email.getText().toString();
+        String passwordUsr = password.getText().toString();
+
+        if (!nombreUsr.isEmpty() && !telefonoUsr.isEmpty() && !emailUsr.isEmpty() && !passwordUsr.isEmpty()){
+            // --------------- si los campos no estan vacios, entonces: --------------
+            //establezco rol
+            Rol rolAdmin = new Rol("ROL_ADMIN");
+            rolDao.postRolAsync(rolAdmin);
+
+            //hasheo contraseña
+            String hashedPassword = HashPass.hashPassword(passwordUsr);
+
+            Usuario usuario = new Usuario();
+            usuario.setNombre_usuario(nombreUsr);
+            usuario.setTelefono_usuario(telefonoUsr);
+            usuario.setEmail_usuario(emailUsr);
+            usuario.setPassword_usuario(hashedPassword); //contraseña hasheada
+            usuario.setRolIdRol(2);
+
+            registroAdminViewModel.postUsuario(usuario);
+
+            Intent intent = new Intent(RegistroAdminActivity.this, LoginActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, "Admin registrado, inicie sesion", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/coworks/ui/registroadmin/RegistroAdminViewModel.java
+++ app/src/main/java/com/example/coworks/ui/registroadmin/RegistroAdminViewModel.java
@@ -1,4 +1,21 @@
 package com.example.coworks.ui.registroadmin;
 
-public class RegistroAdminViewModel {
+import androidx.lifecycle.ViewModel;
+
+import com.example.coworks.data.database.dao.UsuarioDao;
+import com.example.coworks.data.database.model.Usuario;
+
+public class RegistroAdminViewModel extends ViewModel {
+    private UsuarioDao usuarioDao;
+
+    public RegistroAdminViewModel() {
+    }
+
+    public RegistroAdminViewModel(UsuarioDao usuarioDao) {
+        this.usuarioDao = usuarioDao;
+    }
+
+    public void postUsuario(Usuario usuario){
+        usuarioDao.postUserAsync(usuario);
+    }
 }
