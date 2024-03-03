584828bb1e728df6377e0cb2c25f54e70df47d47
Hernan <hernan24744@gmail.com>
2024-03-03 13:53:14 -0300

    Logica para el dao de usuario, gestion para el CRUD

--- app/src/main/java/com/example/coworks/data/database/dao/UsuarioDao.java
+++ app/src/main/java/com/example/coworks/data/database/dao/UsuarioDao.java
@@ -1,4 +1,33 @@
 package com.example.coworks.data.database.dao;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.coworks.data.database.model.Usuario;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Dao
 public interface UsuarioDao {
+
+    @Query("SELECT * FROM usuario WHERE email_usuario= :email AND password_usuario= :password")
+    LiveData<Usuario> getUsuarioCredenciales(String email, String password);
+
+    @Insert
+    void postUsuario(Usuario usuario);
+    static final ExecutorService executorSv = Executors.newSingleThreadExecutor();
+    default void postUserAsync(Usuario usuario){
+        executorSv.execute(()->{postUsuario(usuario);});
+    }
+
+    @Update
+    void updateUsuario(Usuario usuario);
+
+    @Delete
+    void deleteUsuario(Usuario usuario);
 }
