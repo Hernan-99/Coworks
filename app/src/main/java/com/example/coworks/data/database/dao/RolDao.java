9cc07dc59c0a52a7cda7c5f50181c0f18f45b812
Hernan <hernan24744@gmail.com>
2024-03-03 13:51:53 -0300

    contruccion de logica para crear rol de usuario

--- app/src/main/java/com/example/coworks/data/database/dao/RolDao.java
+++ app/src/main/java/com/example/coworks/data/database/dao/RolDao.java
@@ -1,4 +1,20 @@
 package com.example.coworks.data.database.dao;
 
+import androidx.room.Dao;
+import androidx.room.Insert;
+
+import com.example.coworks.data.database.model.Rol;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Dao
 public interface RolDao {
+    @Insert
+    void postRol(Rol rol);
+    static final ExecutorService executorSv = Executors.newSingleThreadExecutor();
+    default void postRolAsync(Rol rol){
+        executorSv.execute(()->{postRol(rol);});
+    }
+
 }
